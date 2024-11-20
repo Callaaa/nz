5c6c572fdfd40b7b076063d69a8f842af4026d98
Callaaa <148789803+Callaaa@users.noreply.github.com>
2024-11-20 11:11:54 +0200

    Add files via upload

---
+++ stari dqdo/stari dqdo/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stari_dqdo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.passingvalue = textBox1.Text;
+            frm2.passingvalue1 = textBox2.Text;
+            frm2.passingvalue2 = textBox3.Text;
+            frm2.ShowDialog();
+        }
+    }
+}
---
+++ stari dqdo/stari dqdo/Form2.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace stari_dqdo
+{
+    public partial class Form2 : Form
+    {
+        public string name;
+        public string email;
+        public string city;
+
+        public string passingvalue
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string passingvalue1
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public string passingvalue2
+        {
+            get { return city; }
+            set { city = value; }
+        }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            label4.Text = "" + name;
+            label5.Text = "" + email;
+            label6.Text = "" + city;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
